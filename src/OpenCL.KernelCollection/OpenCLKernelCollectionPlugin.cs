888e8ad6025e89b74d7938a210d50d8a98f5a378
Tim Akermann <ti.akermann@gmail.com>
2020-09-29 11:24:12 +0200

    Code Cleanup

--- src/OpenCL.KernelCollection/OpenCLKernelCollectionPlugin.cs
+++ src/OpenCL.KernelCollection/OpenCLKernelCollectionPlugin.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
 using PluginSystem.Core;
 using PluginSystem.Core.Interfaces;
 using PluginSystem.Core.Pointer;
 using PluginSystem.Utility;
 
-using Utility.IO.Callbacks;
 using Utility.IO.VirtualFS;
 
 namespace OpenCL.KernelCollection
@@ -14,19 +12,17 @@ namespace OpenCL.KernelCollection
     public class OpenCLKernelCollectionPlugin : APlugin<PluginSystemHost>
     {
 
-
-
         public override void OnLoad(PluginAssemblyPointer ptr)
         {
             base.OnLoad(ptr);
 
             ManifestReader.RegisterAssembly(Assembly.GetExecutingAssembly());
-
         }
 
         public override bool SatisfiesHostType(IPluginHost potentialHost)
         {
-            return !ManifestReader.IsRegistered(Assembly.GetExecutingAssembly()) && base.SatisfiesHostType(potentialHost);
+            return !ManifestReader.IsRegistered(Assembly.GetExecutingAssembly()) &&
+                   base.SatisfiesHostType(potentialHost);
         }
 
     }
