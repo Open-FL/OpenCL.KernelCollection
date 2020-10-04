b9d96db763a37835a24d49f846347e332701afbe
Tim Akermann <ti.akermann@gmail.com>
2020-10-04 15:24:20 +0200

    Added unit tests

---
+++ tests/OpenCL.KernelCollection.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+using OpenCL.KernelCollection;
+using OpenCL.Wrapper;
+using OpenCL.Wrapper.TypeEnums;
+
+using Utility.IO.VirtualFS;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ManifestReader.RegisterAssembly(typeof(OpenCLKernelCollectionPlugin).Assembly);
+            EmbeddedFileIOManager.Initialize();
+        }
+
+        [Test]
+        public void CompileTest()
+        {
+            CLAPI instance = CLAPI.GetInstance();
+            KernelDatabase db = new KernelDatabase(instance, "resources/kernel", DataVectorTypes.Uchar1);
+
+            Assert.True(db.KernelNames.Count != 0);
+
+            db.Dispose();
+            instance.Dispose();
+        }
+    }
+}
